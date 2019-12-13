1da86e131ac74de1c56efd95e91762eb25e693f9
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-12 23:18:37 -0300

    Init in balance card

---
+++ lib/app/modules/home/widgets/balance_card.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:nu_buy/app/shared/colors.dart';
+
+class BalanceCard extends StatefulWidget {
+  @override
+  _BalanceCardState createState() => _BalanceCardState();
+}
+
+class _BalanceCardState extends State<BalanceCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+          padding: EdgeInsets.fromLTRB(20, 70, 20, 30),
+          child: FractionallySizedBox(
+            widthFactor: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: CustomColors.mainWhite,
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                        blurRadius: 6.0,
+                        color: CustomColors.mainShadow,
+                        offset: Offset(0, 3))
+                  ]),
+              child: Column(
+                children: <Widget>[
+                  FractionallySizedBox(
+                    widthFactor: .5,
+                    child: Container(
+                      color: CustomColors.mainSky,
+                      margin: EdgeInsets.only(top: 50),
+                      height: 10,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+}
