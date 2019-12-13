0c38bdb504af6ff7009d7f2e49a1b96dd16a4303
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-12 23:18:26 -0300

    FInished custom navigation card

--- lib/app/modules/home/widgets/custom_card.dart
+++ lib/app/modules/home/widgets/custom_card.dart
@@ -1,39 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:nu_buy/app/shared/colors.dart';
 
-/// This is a custom widget for card that we'll use him like navigator, 
+/// This is a custom widget for card that we'll use him like navigator,
 /// this widget will try to solve all problems using assert
 /// and putting default values to put less text into code of application
 class CustomCard extends StatelessWidget {
   final double width;
-  final double height;
   final Color cardColor;
-  final Text cardText;
-  final Icon cardIcon;
+  final String cardText;
+  final double cardTextSize;
+  final Color cardTextColor;
+  final FontWeight cardTextWeight;
+  final IconData cardIcon;
+  final double cardIconSize;
+  final Color cardIconColor;
   final double cardBorderRadius;
 
-  const CustomCard({this.height = 125, this.width = 150, this.cardColor = CustomColors.mainSky, this.cardText, this.cardBorderRadius = 20, this.cardIcon});
+  const CustomCard(
+      {this.width = 130,
+      this.cardColor = CustomColors.mainSky,
+      this.cardIconColor = CustomColors.mainWhite,
+      this.cardIconSize = 25,
+      @required this.cardIcon,
+      @required this.cardText,
+      this.cardTextSize = 18,
+      this.cardTextWeight = FontWeight.bold,
+      this.cardTextColor = CustomColors.mainWhite,
+      this.cardBorderRadius = 20});
 
   @override
   Widget build(BuildContext context) {
     /// Check if height defined is less than height of context
-    assert(height < MediaQuery.of(context).size.height);
-    return Container(
-      width: this.width,
-      height: this.height,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(this.cardBorderRadius),
-        color: this.cardColor,
-      ),
-      child: Column(
-        children: <Widget>[
-          Flexible(
-            child: this.cardIcon,
-          ),
-          Flexible(
-            child: this.cardText,
-          )
-        ],
+    return FlatButton(
+      onPressed: null,
+      child: Container(
+        width: this.width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(this.cardBorderRadius),
+          color: this.cardColor,
+        ),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+                child: Padding(
+              padding: EdgeInsets.only(top: 20, right: 20),
+              child: Align(
+                alignment: FractionalOffset.topRight,
+                child: Icon(
+                  this.cardIcon,
+                  color: this.cardIconColor,
+                  size: this.cardIconSize,
+                ),
+              ),
+            )),
+            Expanded(
+                child: Padding(
+              padding: EdgeInsets.only(bottom: 20, left: 20),
+              child: Align(
+                alignment: FractionalOffset.bottomLeft,
+                child: Text(
+                  this.cardText,
+                  style: TextStyle(
+                      fontSize: this.cardTextSize,
+                      color: this.cardTextColor,
+                      fontWeight: this.cardTextWeight),
+                ),
+              ),
+            ))
+          ],
+        ),
       ),
     );
   }
