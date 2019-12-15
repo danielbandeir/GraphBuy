72c4488c20918e57d736dc2713082a422108c9d1
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-15 15:23:52 -0300

    changed appmain wiwdget

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:nubuy/app/modules/home/home_module.dart';
 
 class AppWidget extends StatelessWidget {
@@ -10,6 +11,9 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
     return CupertinoApp(
       title: 'NuBuy',
       initialRoute: this.initialRoute,
