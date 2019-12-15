c2bbac286c30ee74eb2d4cf64c7f4ba255d97ab7
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-15 02:59:26 -0300

    Added isVisible to balance in HomeBloc

--- lib/app/modules/home/home_bloc.dart
+++ lib/app/modules/home/home_bloc.dart
@@ -1,9 +1,13 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:rxdart/subjects.dart';
 
 class HomeBloc extends BlocBase {
+  BehaviorSubject<bool> isVisible = new BehaviorSubject<bool>();
+  
   //dispose will be called automatically by closing its streams
   @override
   void dispose() {
+    isVisible.close();
     super.dispose();
   }
 }
