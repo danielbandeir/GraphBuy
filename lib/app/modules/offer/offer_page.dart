8ac1ff59b23214895ff783118532dc9206463bf9
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-15 03:01:26 -0300

    Init the ofer page widget

--- lib/app/modules/offer/offer_page.dart
+++ lib/app/modules/offer/offer_page.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
 class OfferPage extends StatefulWidget {
-  final String title;
-  const OfferPage({Key key, this.title = "Offer"}) : super(key: key);
-
   @override
   _OfferPageState createState() => _OfferPageState();
 }
@@ -12,9 +9,6 @@ class _OfferPageState extends State<OfferPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Column(
         children: <Widget>[
           Container(
