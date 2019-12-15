2bf8635e4712a099cfbd69731d051de4ef1cdc30
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-15 02:58:51 -0300

    Changed constructor in AppModule

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -2,6 +2,7 @@ import 'package:nubuy/app/app_bloc.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:nubuy/app/app_widget.dart';
+import 'package:nubuy/app/core/routes.dart';
 
 class AppModule extends ModuleWidget {
   @override
@@ -13,7 +14,7 @@ class AppModule extends ModuleWidget {
   List<Dependency> get dependencies => [];
 
   @override
-  Widget get view => AppWidget();
+  Widget get view => AppWidget(initialRoute: indexRoute, routes: routes);
 
   static Inject get to => Inject<AppModule>.of();
 }
