e9d47d8bcdcb6cab9ba9db3e80f0e710c5324b4d
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-16 00:13:10 -0300

    Added animated container in cart_bottom

--- lib/app/modules/cart/widgets/cart_bottom.dart
+++ lib/app/modules/cart/widgets/cart_bottom.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nubuy/app/modules/cart/cart_bloc.dart';
+import 'package:nubuy/app/modules/cart/cart_module.dart';
+import 'package:nubuy/app/shared/colors.dart';
 
 class CartBottom extends StatefulWidget {
   @override
@@ -6,18 +9,53 @@ class CartBottom extends StatefulWidget {
 }
 
 class _CartBottomState extends State<CartBottom> {
+  final cartBloc = CartModule.to.bloc<CartBloc>();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 80,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topRight: Radius.circular(40),
-          topLeft: Radius.circular(40)
-        )
-      ),
+    return StreamBuilder<bool>(
+      stream: cartBloc.cardToggle.stream,
+      initialData: false,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return AnimatedContainer(
+          duration: Duration(milliseconds: 500),
+          curve: Curves.fastOutSlowIn,
+          width: double.infinity,
+          height: snapshot.data ? 250 : 80,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(40), topLeft: Radius.circular(40))),
+          child: Padding(
+            padding: EdgeInsets.only(left: 40, right: 40, top: 20),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text("Valor: 3000,00"),
+                    Spacer(),
+                    IconButton(
+                        icon: Icon(snapshot.data ? Icons.arrow_downward : Icons.arrow_upward),
+                        onPressed: () => cartBloc.cardToggle.add(snapshot.data ? false : true))
+                  ],
+                ),
+                snapshot.data ? AnimatedContainer(
+                  duration: Duration(seconds: 2),
+                  child: Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: RaisedButton(
+                    onPressed: null,
+                    
+                    child: Text("Pagar"),
+                    color: CustomColors.mainGreen,
+                  ),
+                ),
+                ) : Spacer(),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
