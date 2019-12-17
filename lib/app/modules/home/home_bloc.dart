407f4a383302dad342768706809bb8ae5c7f4140
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-17 15:36:36 -0300

    Home bloc removed some itens like getUserInfo

--- lib/app/modules/home/home_bloc.dart
+++ lib/app/modules/home/home_bloc.dart
@@ -1,13 +1,10 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
-import 'package:rxdart/subjects.dart';
 
 class HomeBloc extends BlocBase {
-  BehaviorSubject<bool> isVisible = new BehaviorSubject<bool>.seeded(false);
   
   //dispose will be called automatically by closing its streams
   @override
   void dispose() {
-    isVisible.close();
     super.dispose();
   }
 }
