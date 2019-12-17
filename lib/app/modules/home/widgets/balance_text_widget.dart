b48f3988a941424cb8d363b4a1e4190e28401817
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-17 15:38:17 -0300

    Removed some bloc

--- lib/app/modules/home/widgets/balance_text_widget.dart
+++ lib/app/modules/home/widgets/balance_text_widget.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:nubuy/app/modules/home/home_bloc.dart';
-import 'package:nubuy/app/modules/home/home_module.dart';
+import 'package:nubuy/app/app_bloc.dart';
+import 'package:nubuy/app/app_module.dart';
 import 'package:nubuy/app/shared/colors.dart';
+import 'package:nubuy/app/shared/models/user.model.dart';
 
 class BalacenTextWidget extends StatefulWidget {
   @override
@@ -10,70 +11,74 @@ class BalacenTextWidget extends StatefulWidget {
 }
 
 class _BalacenTextWidgetState extends State<BalacenTextWidget> {
-  final bloc = HomeModule.to.bloc<HomeBloc>();
+  final AppBloc bloc = AppModule.to.bloc<AppBloc>();
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Padding(
-        padding: EdgeInsets.only(left: 70, right: 70),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(bottom: 20),
-                  child: Align(
-                    alignment: AlignmentDirectional.center,
-                    child: Text(
-                      "Olá, Daniel",
-                      style: TextStyle(
-                        color: CustomColors.mainGrey,
-                        fontWeight: FontWeight.w600,
-                        fontSize: 28,
-                      ),
+    return FutureBuilder<UserModel>(
+      future: bloc.getUser(),
+      builder: (BuildContext context, snapshotUser) {
+        if(snapshotUser.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          return Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(left: 70, right: 70),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 20),
+                    child: Align(
+                      alignment: AlignmentDirectional.center,
+                      child: Text(snapshotUser.data.name),
                     ),
                   ),
-                ),
-            Row(
-              children: <Widget>[
-                Align(
-                  child: Text(
-                    "Saldo",
-                    style: TextStyle(color: CustomColors.mainGrey),
-                  ),
-                ),
-                Align(
-                  child: IconButton(
-                    onPressed: () => bloc.isVisible.add(!bloc.isVisible.value),
-                    color: CustomColors.mainGrey,
-                    icon: const Icon(Icons.visibility_off),
-                  ),
-                ),
-              ],
-            ),
-            Align(
-              alignment: AlignmentDirectional.centerEnd,
-              child: StreamBuilder(
-                stream: bloc.isVisible.stream,
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  return AnimatedOpacity(
-                    duration: Duration(milliseconds: 100),
-                    opacity: !snapshot.data ? 1 : 0,
-                    child: Text(
-                      "RS 3000,00",
-                      style: TextStyle(
-                        color: CustomColors.mainGrey,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24,
+                  Row(
+                    children: <Widget>[
+                      Align(
+                        child: Text(
+                          "Saldo",
+                          style: TextStyle(color: CustomColors.mainGrey),
+                        ),
                       ),
+                      Align(
+                        child: IconButton(
+                          onPressed: () =>
+                              bloc.isVisible.add(!bloc.isVisible.value),
+                          color: CustomColors.mainGrey,
+                          icon: const Icon(Icons.visibility_off),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Align(
+                    alignment: AlignmentDirectional.centerEnd,
+                    child: StreamBuilder(
+                      stream: bloc.isVisible.stream,
+                      initialData: false,
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                        return AnimatedOpacity(
+                          duration: Duration(milliseconds: 100),
+                          opacity: !snapshot.data ? 1 : 0,
+                          child: Text(
+                            snapshotUser.data.balance.toString(),
+                            style: TextStyle(
+                              color: CustomColors.mainGrey,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                            ),
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
+                  )
+                ],
               ),
-            )
-          ],
-        ),
-      ),
+            ),
+          );
+        }
+      },
     );
   }
 }
