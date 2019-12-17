faf47ec536daf3eb64dc2e7f5dfed4d4b5ce8dd0
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-16 21:21:32 -0300

    Initialize with seeded / if have time put with hive

--- lib/app/modules/home/home_bloc.dart
+++ lib/app/modules/home/home_bloc.dart
@@ -2,7 +2,7 @@ import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:rxdart/subjects.dart';
 
 class HomeBloc extends BlocBase {
-  BehaviorSubject<bool> isVisible = new BehaviorSubject<bool>();
+  BehaviorSubject<bool> isVisible = new BehaviorSubject<bool>.seeded(false);
   
   //dispose will be called automatically by closing its streams
   @override
