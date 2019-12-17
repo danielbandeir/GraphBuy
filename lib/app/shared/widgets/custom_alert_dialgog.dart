da0952ce0294abbab2a1b57d391588c2568fff2c
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-17 15:38:32 -0300

    Make custom alert dialog

--- lib/app/shared/widgets/custom_alert_dialgog.dart
+++ lib/app/shared/widgets/custom_alert_dialgog.dart
@@ -2,9 +2,19 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nubuy/app/shared/widgets/custom_content_dialog.dart';
 
-void showDescription(BuildContext context, String title, double value, String description, String textRaised, Function onPressRaised) async {
+void showDescription(BuildContext context, String title, double value,
+    String description, String textRaised, Function onPressRaised,
+    {double heightContent,
+    List<double> marginsAll,
+    double marginIconText,
+    List<double> marginsValueText,
+    double sizeDescription,
+    double distanceManyButton,
+    Color raisedButtonColor,
+    double distancecDescMany,
+    int numberItems}) async {
   showModalBottomSheet(
-    context: context,// user must tap button!
+    context: context, // user must tap button!
     builder: (BuildContext context) {
       return CustomContentDialog(
         title: title,
@@ -12,6 +22,15 @@ void showDescription(BuildContext context, String title, double value, String de
         description: description,
         raisedButtonText: textRaised,
         onPressedRaisedButton: onPressRaised,
+        heightContent: heightContent,
+        marginsAll: marginsAll,
+        marginIconText: marginIconText,
+        marginsValueText: marginsValueText,
+        sizeDescription: sizeDescription,
+        distanceManyButton: distanceManyButton,
+        distanceDescMany: distancecDescMany,
+        raisedButtonColor: raisedButtonColor,
+        numberItems: numberItems,
       );
     },
   );
