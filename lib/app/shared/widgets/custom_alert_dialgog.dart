4d230c7eb8926daaa049583b9018a8024f0b595c
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-16 21:24:09 -0300

    Created custom alert dialog

--- lib/app/shared/widgets/custom_alert_dialgog.dart
+++ lib/app/shared/widgets/custom_alert_dialgog.dart
@@ -2,11 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nubuy/app/shared/widgets/custom_content_dialog.dart';
 
-void showDescription(BuildContext context) async {
+void showDescription(BuildContext context, String title, double value, String description) async {
   showModalBottomSheet(
     context: context,// user must tap button!
     builder: (BuildContext context) {
-      return CustomContentDialog();
+      return CustomContentDialog(
+        title: title,
+        value: value,
+        description: description,
+      );
     },
   );
 }
