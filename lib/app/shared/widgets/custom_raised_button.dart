e8f0b9e9d5f4fba63c732b83cfeda35f05229037
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-16 22:42:05 -0300

    Created generic custom raised button

---
+++ lib/app/shared/widgets/custom_raised_button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:nubuy/app/shared/colors.dart';
+
+class CustomRaisedButton extends StatefulWidget {
+  final String raisedButtonText;
+  final Function onPressedRaisedButton;
+  final double distanceManyButton;
+  final Color raisedButtonColor;
+  final String raisedText;
+  final Color raisedTextColor;
+  final double raisedFontSize;
+
+  const CustomRaisedButton({
+      @required this.raisedButtonText,
+      @required this.onPressedRaisedButton,
+      this.raisedButtonColor = CustomColors.mainGreen,
+      this.distanceManyButton = 10,
+      this.raisedText = "Salvar",
+      this.raisedTextColor = CustomColors.mainWhite,
+      this.raisedFontSize = 18
+  });
+  
+  @override
+  _CustomRaisedButtonState createState() => _CustomRaisedButtonState();
+}
+
+class _CustomRaisedButtonState extends State<CustomRaisedButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+            padding: EdgeInsets.only(top: widget.distanceManyButton),
+            child: RaisedButton(
+              onPressed: widget.onPressedRaisedButton,
+              color: widget.raisedButtonColor,
+              child: Center(
+                child: Text(
+                  widget.raisedText,
+                  style: TextStyle(
+                    color: widget.raisedTextColor,
+                    fontSize: widget.raisedFontSize
+                  ),
+                ),
+              ),
+            ),
+          );
+  }
+}
