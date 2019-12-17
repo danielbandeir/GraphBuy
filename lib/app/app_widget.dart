14e935b821bd25fdd7124cbdea5f983b89817d37
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-17 15:33:57 -0300

    Changed instance into app_widget

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:nubuy/app/app_module.dart';
 import 'package:nubuy/app/modules/home/home_module.dart';
 
 class AppWidget extends StatelessWidget {
@@ -11,14 +13,20 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final clientGraphQl = AppModule.to.getDependency<ValueNotifier>();
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
     ));
-    return MaterialApp(
-      title: 'NuBuy',
-      initialRoute: this.initialRoute,
-      routes: this.routes,
-      home: HomeModule(),
+    return GraphQLProvider(
+      client: clientGraphQl,
+      child: CacheProvider(
+        child: MaterialApp(
+          title: 'NuBuy',
+          initialRoute: this.initialRoute,
+          routes: this.routes,
+          home: HomeModule(),
+        ),
+      ),
     );
   }
 }
