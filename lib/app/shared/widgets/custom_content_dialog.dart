afffd7e56b5db18efe01a408eaa9625cb599a333
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-16 22:41:50 -0300

    Added some params in custom content

--- lib/app/shared/widgets/custom_content_dialog.dart
+++ lib/app/shared/widgets/custom_content_dialog.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nubuy/app/shared/colors.dart';
+import 'package:nubuy/app/shared/widgets/custom_raised_button.dart';
 
 class CustomContentDialog extends StatefulWidget {
   final String title;
@@ -6,19 +8,29 @@ class CustomContentDialog extends StatefulWidget {
   final String description;
   final double heightContent;
   final List<double> marginsAll;
-  final double marginBetweenIconText;
+  final double marginIconText;
   final List<double> marginsValueText;
   final double sizeDescription;
+  final String raisedButtonText;
+  final Function onPressedRaisedButton;
+  final double distanceManyButton;
+  final Color raisedButtonColor;
+  final double distanceDescMany;
 
   const CustomContentDialog(
       {@required this.title,
       @required this.value,
       @required this.description,
+      @required this.raisedButtonText,
+      @required this.onPressedRaisedButton,
+      this.raisedButtonColor = CustomColors.mainGreen,
+      this.distanceManyButton = 10,
       this.heightContent = 400,
+      this.distanceDescMany = 20,
       this.marginsAll = const <double>[40.0, 40.0, 30.0],
-      this.marginBetweenIconText = 20,
+      this.marginIconText = 20,
       this.marginsValueText = const <double> [20, 20],
-      this.sizeDescription = 150});
+      this.sizeDescription = 100}) : assert(heightContent >= 400);
 
   @override
   _CustomContentDialogState createState() => _CustomContentDialogState();
@@ -46,7 +58,7 @@ class _CustomContentDialogState extends State<CustomContentDialog> {
                 ),
               ),
               Padding(
-                padding: EdgeInsets.only(left: widget.marginBetweenIconText),
+                padding: EdgeInsets.only(left: widget.marginIconText),
                 child: Text(widget.title),
               )
             ],
@@ -67,6 +79,28 @@ class _CustomContentDialogState extends State<CustomContentDialog> {
                 Text(widget.description)
               ],
             ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: widget.distanceDescMany),
+            child: RaisedButton(
+              onPressed: widget.onPressedRaisedButton,
+              color: widget.raisedButtonColor,
+              child: Center(
+                child: Text(
+                  "Salvar",
+                  style: TextStyle(
+                    color: CustomColors.mainWhite,
+                    fontSize: 18
+                  ),
+                ),
+              ),
+            ),
+          ),
+          CustomRaisedButton(
+            onPressedRaisedButton: widget.onPressedRaisedButton,
+            raisedButtonText: widget.raisedButtonText,
+            distanceManyButton: widget.distanceManyButton,
+            raisedButtonColor: widget.raisedButtonColor,
           )
         ],
       ),
