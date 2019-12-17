1dafe551ffd1cd367468cd8159771ec2aa19e6b7
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-17 15:32:30 -0300

    Put getInfoUser into appBloc

--- lib/app/app_bloc.dart
+++ lib/app/app_bloc.dart
@@ -1,9 +1,20 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:nubuy/app/app_module.dart';
+import 'package:nubuy/app/app_repository.dart';
+import 'package:nubuy/app/shared/models/user.model.dart';
+import 'package:rxdart/rxdart.dart';
 
 class AppBloc extends BlocBase {
-  //dispose will be called automatically by closing its streams
+  final AppRepository appRepository = AppModule.to.getDependency<AppRepository>();
+  final BehaviorSubject<bool> isVisible = new BehaviorSubject<bool>.seeded(false);
+  final BehaviorSubject userControl = BehaviorSubject<UserModel>();
+
+  Future<UserModel> getUser() async => await appRepository.getUserInfo();
+
   @override
   void dispose() {
+    isVisible.close();
+    userControl.close();
     super.dispose();
   }
 }
