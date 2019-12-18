f4f6a92f7bac1f84af94556dbe065eedfdb2b841
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 02:12:06 -0300

    Created implementation for home repository

---
+++ lib/app/modules/home/impl/home_repository_impl.dart
@@ -0,0 +1,33 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:graphql/client.dart';
+import 'package:nubuy/app/modules/home/home_repository.dart';
+import 'package:nubuy/app/shared/models/user.model.dart';
+
+class HomeRepositoryImpl extends Disposable implements HomeRepository {
+  final GraphQLClient client;
+
+  HomeRepositoryImpl({this.client});
+
+  @override
+  Future<UserModel> getUserInfo() async {
+    const String query = '''
+      query getUser {
+        viewer {
+             id
+             name
+             balance
+          }
+      }
+    ''';
+    final QueryOptions options = QueryOptions(
+        documentNode: gql(query)
+    );
+
+    final QueryResult result = await client.query(options);
+    UserModel user = UserModel.fromJson(result.data["viewer"]);
+    return user;
+  }
+
+  @override
+  void dispose() {}
+}
