9b443fff78c12045b4f06e9b74bafa3e177c126f
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-17 22:25:25 -0300

    Added purchase model and purchase costumer model

---
+++ lib/app/shared/models/purchase.model.dart
@@ -0,0 +1,23 @@
+class PurchaseModel {
+  final bool success;
+  final String errorMessage;
+  final CustomerModel customer;
+
+  const PurchaseModel({
+    this.success,
+    this.errorMessage,
+    this.customer,
+  });
+
+  factory PurchaseModel.fromJson(Map<String, dynamic> json) => PurchaseModel(
+    success: json["success"],
+    errorMessage: json["errorMessage"],
+    customer: CustomerModel.fromJson(json["customer"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "success": success,
+    "errorMessage": errorMessage,
+    "customer": customer.toJson(),
+  };
+}
