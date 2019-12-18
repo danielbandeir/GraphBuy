6d40eff440dd4782f038c520c37ea504b67191ec
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-17 21:44:37 -0300

    Added dependency injection

--- lib/app/modules/offer/offer_module.dart
+++ lib/app/modules/offer/offer_module.dart
@@ -1,16 +1,25 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:nubuy/app/app_module.dart';
 import 'package:nubuy/app/modules/offer/offer_bloc.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:nubuy/app/modules/offer/offer_page.dart';
+import 'package:nubuy/app/modules/offer/offer_repository.dart';
 
 class OfferModule extends ModuleWidget {
   @override
   List<Bloc> get blocs => [
-        Bloc((i) => OfferBloc()),
+        Bloc((i) => OfferBloc(
+          offerRepository: OfferModule.to.getDependency<OfferRepository>()
+        )),
       ];
 
   @override
-  List<Dependency> get dependencies => [];
+  List<Dependency> get dependencies => [
+    Dependency((i) => OfferRepository(
+      client: AppModule.to.getDependency<GraphQLClient>()
+    ))
+  ];
 
   @override
   Widget get view => OfferPage();
