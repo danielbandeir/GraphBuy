c843c1194786a80a79eeac0b87a2894b79bbe16a
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 17:30:59 -0300

    Changed historic body

---
+++ lib/app/modules/historic/widgets/historic_body.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:nubuy/app/shared/colors.dart';
+import 'package:nubuy/app/shared/widgets/custom_alert_dialgog.dart';
+import 'package:nubuy/app/shared/widgets/custom_app_bar.dart';
+import 'package:nubuy/app/shared/widgets/custom_list_tile.dart';
+
+class HistoricBody extends StatefulWidget {
+  @override
+  _HistoricBodyState createState() => _HistoricBodyState();
+}
+
+class _HistoricBodyState extends State<HistoricBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        CustomAppBar(
+          title: "Histórico",
+          iconButtonRedirect: Spacer(),
+        ),
+        Expanded(
+            child: WatchBoxBuilder(
+          box: Hive.box('historic'),
+          builder: (context, box) {
+            return Container(
+              decoration: BoxDecoration(
+                color: CustomColors.mainSky,
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(left: 20, right: 20, top: 5),
+                child: ListView.builder(
+                  itemCount: box.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return CustomListTile(
+                      onPressed: () => showDescription(
+                          context,
+                          box.getAt(index).product.name,
+                          box.getAt(index).price,
+                          box.getAt(index).product.description,
+                          box.getAt(index).product.image,
+                          "Comprar",
+                          () => print("test")),
+                      title: box.getAt(index).product.name,
+                      price: box.getAt(index).price,
+                      image: box.getAt(index).product.image,
+                    );
+                  },
+                ),
+              ),
+            );
+          },
+        )),
+      ],
+    );
+  }
+}
