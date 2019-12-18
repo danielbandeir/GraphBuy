0b97bbe516191cc61313f7e1d8a2f09bea3d4c9f
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 00:00:23 -0300

    Removed instances in app_bloc

--- lib/app/app_bloc.dart
+++ lib/app/app_bloc.dart
@@ -1,20 +1,9 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
-import 'package:nubuy/app/app_module.dart';
-import 'package:nubuy/app/app_repository.dart';
-import 'package:nubuy/app/shared/models/user.model.dart';
-import 'package:rxdart/rxdart.dart';
 
 class AppBloc extends BlocBase {
-  final AppRepository appRepository = AppModule.to.getDependency<AppRepository>();
-  final BehaviorSubject<bool> isVisible = new BehaviorSubject<bool>.seeded(false);
-  final BehaviorSubject userControl = BehaviorSubject<UserModel>();
-
-  Future<UserModel> getUser() async => await appRepository.getUserInfo();
 
   @override
   void dispose() {
-    isVisible.close();
-    userControl.close();
     super.dispose();
   }
 }
