28871379c2a4a23f29cd536a70f4a03a4f4d5a87
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 05:50:28 -0300

    Updated offer page to check connection status

--- lib/app/modules/offer/offer_page.dart
+++ lib/app/modules/offer/offer_page.dart
@@ -1,6 +1,8 @@
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:nubuy/app/modules/offer/widgets/offer_body_page.dart';
 import 'package:nubuy/app/shared/colors.dart';
+import 'package:nubuy/app/shared/widgets/mobile_offline.dart';
 
 class OfferPage extends StatefulWidget {
   @override
@@ -12,7 +14,12 @@ class _OfferPageState extends State<OfferPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: CustomColors.mainSky,
-      body: OfferBodyPage(),
+      body: StreamBuilder<ConnectivityResult>(
+        stream: Connectivity().onConnectivityChanged,
+        builder: (BuildContext context, AsyncSnapshot<ConnectivityResult> snapshot) {
+          return snapshot.data != ConnectivityResult.none ? OfferBodyPage() : MobileOffline();
+        },
+      ),
     );
   }
 }
