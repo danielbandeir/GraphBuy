641b71eb9be9df20b379467e853dc5a3b3048ef1
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 02:11:19 -0300

    Added abstract classes and his implementations

---
+++ lib/app/modules/offer/impl/offer_repository_impl.dart
@@ -0,0 +1,68 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:graphql/client.dart';
+import 'package:nubuy/app/modules/offer/offer_repository.dart';
+import 'package:nubuy/app/shared/models/offers.model.dart';
+import 'package:nubuy/app/shared/models/purchase.model.dart';
+
+class OfferRepositoryImpl extends Disposable implements OfferRepository {
+  final GraphQLClient client;
+
+  OfferRepositoryImpl({this.client});
+
+  @override
+  Future<List<OfferModel>> getUserOffer() async {
+    const String query = '''
+    {
+      viewer {
+        offers {
+          id
+          price
+          product {
+            id
+            name
+            description
+            image
+          }
+        }
+      }
+    }
+    ''';
+    final QueryOptions options = QueryOptions(
+        documentNode: gql(query),
+
+    );
+    final QueryResult result = await client.query(options);
+    List<OfferModel> userOffers = OfferModel.fromJsonList(await result.data["viewer"]["offers"]);
+    return userOffers;
+  }
+
+  @override
+  Future<PurchaseModel> purchaseOffer(String id) async {
+    const String tryPurchase = r'''
+    mutation tryPurcgase($id: ID!) {
+      purchase(offerId: $id) {
+        success,
+        errorMessage,
+        customer {
+          id
+          balance
+        }
+      }
+    }
+    ''';
+
+    final MutationOptions options = MutationOptions(
+      documentNode: gql(tryPurchase),
+      variables: <String, dynamic>{
+        'id': id,
+      },
+    );
+
+    final QueryResult result = await client.mutate(options);
+    PurchaseModel purchaseModel = PurchaseModel.fromJson(await result.data["purchase"]);
+    return purchaseModel;
+  }
+
+  @override
+  void dispose() {}
+}
--- lib/app/modules/offer/offer_repository.dart
+++ lib/app/modules/offer/offer_repository.dart
@@ -1,39 +1,7 @@
-import 'package:bloc_pattern/bloc_pattern.dart';
-import 'package:graphql/client.dart';
 import 'package:nubuy/app/shared/models/offers.model.dart';
-import 'package:nubuy/app/shared/models/user.model.dart';
+import 'package:nubuy/app/shared/models/purchase.model.dart';
 
-class OfferRepository extends Disposable {
-  final GraphQLClient client;
-
-  OfferRepository({this.client});
-
-  Future<List<OfferModel>> getUserOffer() async {
-    const String query = '''
-    {
-      viewer {
-        offers {
-          id
-          price
-          product {
-            id
-            name
-            description
-            image
-          }
-        }
-      }
-    }
-    ''';
-    final QueryOptions options = QueryOptions(
-        documentNode: gql(query),
-
-    );
-    final QueryResult result = await client.query(options);
-    List<OfferModel> userOffers = OfferModel.fromJsonList(await result.data["viewer"]["offers"]);
-    return userOffers;
-  }
-
-  @override
-  void dispose() {}
+abstract class OfferReṕository {
+  Future<List<OfferModel>> getUserOffer();
+  Future<PurchaseModel> purchaseOffer(String id);
 }
