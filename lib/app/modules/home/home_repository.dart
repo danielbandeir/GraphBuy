9cf1716855e63996f3a1989e3e1c3a2ce7c26318
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 00:03:26 -0300

    Generated home_repository

---
+++ lib/app/modules/home/home_repository.dart
@@ -0,0 +1,31 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:graphql/client.dart';
+import 'package:nubuy/app/shared/models/user.model.dart';
+
+class HomeRepository extends Disposable {
+  final GraphQLClient client;
+
+  HomeRepository({this.client});
+
+  Future<UserModel> getUserInfo() async {
+    const String query = '''
+      query getUser {
+        viewer {
+             id
+             name
+             balance
+          }
+      }
+    ''';
+    final QueryOptions options = QueryOptions(
+        documentNode: gql(query)
+    );
+
+    final QueryResult result = await client.query(options);
+    UserModel user = UserModel.fromJson(result.data["viewer"]);
+    return user;
+  }
+
+  @override
+  void dispose() {}
+}
