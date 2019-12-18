769cffff0185e9494809446d8aa8c8c735add156
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-17 21:44:24 -0300

    Added getOffer

--- lib/app/modules/offer/offer_bloc.dart
+++ lib/app/modules/offer/offer_bloc.dart
@@ -1,6 +1,15 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:nubuy/app/modules/offer/offer_repository.dart';
+import 'package:nubuy/app/shared/models/offers.model.dart';
 
 class OfferBloc extends BlocBase {
+  final OfferRepository offerRepository;
+
+  OfferBloc({this.offerRepository});
+
+
+  Future<List<OfferModel>> getOffers() async => await offerRepository.getUserOffer();
+
   //dispose will be called automatically by closing its streams
   @override
   void dispose() {
