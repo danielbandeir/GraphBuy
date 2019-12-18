4f3327802dd8a1d25e0db4f8f7360ed9803ba2c2
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-17 21:45:06 -0300

    Changed offer body to add to remove mocked items

--- lib/app/modules/offer/widgets/offer_body_page.dart
+++ lib/app/modules/offer/widgets/offer_body_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:nubuy/app/modules/offer/widgets/offer_dialog.dart';
+import 'package:nubuy/app/modules/offer/offer_bloc.dart';
+import 'package:nubuy/app/modules/offer/offer_module.dart';
 import 'package:nubuy/app/shared/colors.dart';
+import 'package:nubuy/app/shared/models/offers.model.dart';
 import 'package:nubuy/app/shared/widgets/custom_alert_dialgog.dart';
 import 'package:nubuy/app/shared/widgets/custom_app_bar.dart';
 import 'package:nubuy/app/shared/widgets/custom_list_tile.dart';
@@ -12,6 +14,8 @@ class OfferBodyPage extends StatefulWidget {
 }
 
 class _OfferBodyPageState extends State<OfferBodyPage> {
+  final OfferBloc offerBloc = OfferModule.to.getBloc<OfferBloc>();
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -25,41 +29,33 @@ class _OfferBodyPageState extends State<OfferBodyPage> {
           ),
         ),
         Expanded(
-          child: Container(
-            decoration: BoxDecoration(
-                color: CustomColors.thirdaryGrey,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40),
-                    topRight: Radius.circular(40))),
-            child: Padding(
-              padding: EdgeInsets.only(left: 20, right: 20, top: 5),
-              child: ListView(
-                children: <Widget>[
-                  CustomListTile(
-                    onPressed: () => showDescription(context, "Teste1", 4000, "Testandsaokdasoksda", "Salvar", () => print("teste")),
-                    title: "Testando",
-                    price: 500.0,
+          child: FutureBuilder<List<OfferModel>>(
+            future: offerBloc.getOffers(),
+            builder: (BuildContext context, snapshot) {
+              return !snapshot.hasData ? Center(child: CircularProgressIndicator()) : Container(
+                decoration: BoxDecoration(
+                    color: CustomColors.thirdaryGrey,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        topRight: Radius.circular(40))),
+                child: Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20, top: 5),
+                  child: ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return CustomListTile(
+                        onPressed: () => showDescription(context, snapshot.data[index].product.name, snapshot.data[index].price, snapshot.data[index].product.description, snapshot.data[index].product.image, "Comprar", () => print("teste")),
+                        title: snapshot.data[index].product.name,
+                        price: snapshot.data[index].price,
+                        image: snapshot.data[index].product.image,
+                      );
+                    },
                   ),
-                  CustomListTile(
-                    onPressed: () => showDescription(context, "Teste1", 4000, "Testandsaokdasoksda", "Salvar", () => print("teste")),
-                    title: "Testando",
-                    price: 500.0,
-                  ),
-                  CustomListTile(
-                    onPressed: () => showDescription(context, "Teste1", 4000, "Testandsaokdasoksda", "Salvar", () => print("teste")),
-                    title: "Testando",
-                    price: 500.0,
-                  ),
-                  CustomListTile(
-                    onPressed: () => showDescription(context, "Teste1", 4000, "Testandsaokdasoksda", "Salvar", () => print("teste")),
-                    title: "Testando",
-                    price: 500.0,
-                  ),
-                ],
-              ),
-            ),
+                ),
+              );
+            },
           ),
-        )
+        ),
       ],
     );
   }
