bb6cc1c981131c1cb993b60afc3e32e29fb66099
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 17:30:41 -0300

    Added historic route

--- lib/app/core/routes.dart
+++ lib/app/core/routes.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:nubuy/app/modules/historic/historic_module.dart';
 import 'package:nubuy/app/modules/home/home_module.dart';
 import 'package:nubuy/app/modules/offer/offer_module.dart';
 
 final Map<String, WidgetBuilder> routes = {
   '/home/main': (BuildContext context) => HomeModule(),
   '/offers/main': (BuildContext context) => OfferModule(),
+  '/historic/main': (BuildContext context) => HistoricModule()
 };
 
 String get indexRoute => '/home/main';
