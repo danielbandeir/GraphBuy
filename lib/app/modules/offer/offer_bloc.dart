0c1fa5993e3ecda501ed188566df5606dbf56ac4
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 02:13:17 -0300

    added function purchase item

--- lib/app/modules/offer/offer_bloc.dart
+++ lib/app/modules/offer/offer_bloc.dart
@@ -2,6 +2,8 @@ import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:nubuy/app/modules/offer/offer_repository.dart';
 import 'package:nubuy/app/shared/models/offers.model.dart';
 
+import 'package:nubuy/app/shared/models/purchase.model.dart';
+
 class OfferBloc extends BlocBase {
   final OfferRepository offerRepository;
 
@@ -9,6 +11,8 @@ class OfferBloc extends BlocBase {
 
 
   Future<List<OfferModel>> getOffers() async => await offerRepository.getUserOffer();
+
+  Future<PurchaseModel> purchaseItem(String id) async => await offerRepository.purchaseOffer(id);
 
   //dispose will be called automatically by closing its streams
   @override
