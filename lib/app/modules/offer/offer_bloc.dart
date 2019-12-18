86af9872f01bd945b1da134e4f9dbbec764c9d73
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 05:50:11 -0300

    Changed some things in offer_bloc

--- lib/app/modules/offer/offer_bloc.dart
+++ lib/app/modules/offer/offer_bloc.dart
@@ -1,13 +1,17 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:connectivity/connectivity.dart';
 import 'package:nubuy/app/modules/offer/offer_repository.dart';
 import 'package:nubuy/app/shared/models/offers.model.dart';
 
 import 'package:nubuy/app/shared/models/purchase.model.dart';
+import 'package:rxdart/rxdart.dart';
 
 class OfferBloc extends BlocBase {
   final OfferRepository offerRepository;
 
   OfferBloc({this.offerRepository});
+
+  final BehaviorSubject<ConnectivityResult> connect = BehaviorSubject<ConnectivityResult>();
 
 
   Future<List<OfferModel>> getOffers() async => await offerRepository.getUserOffer();
@@ -17,6 +21,7 @@ class OfferBloc extends BlocBase {
   //dispose will be called automatically by closing its streams
   @override
   void dispose() {
+    connect.close();
     super.dispose();
   }
 }
