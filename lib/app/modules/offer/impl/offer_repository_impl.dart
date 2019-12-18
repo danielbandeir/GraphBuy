4139ec8fdc1a72c3d5eef5d48efe087cc5ad3122
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 17:31:34 -0300

    Added hive to implementation repository

--- lib/app/modules/offer/impl/offer_repository_impl.dart
+++ lib/app/modules/offer/impl/offer_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:graphql/client.dart';
+import 'package:hive/hive.dart';
 import 'package:nubuy/app/modules/offer/offer_repository.dart';
 import 'package:nubuy/app/shared/models/offers.model.dart';
 import 'package:nubuy/app/shared/models/purchase.model.dart';
@@ -37,7 +38,7 @@ class OfferRepositoryImpl extends Disposable implements OfferRepository {
   }
 
   @override
-  Future<PurchaseModel> purchaseOffer(String id) async {
+  Future<PurchaseModel> purchaseOffer(String id, OfferModel offer) async {
     const String tryPurchase = r'''
     mutation tryPurcgase($id: ID!) {
       purchase(offerId: $id) {
@@ -59,8 +60,13 @@ class OfferRepositoryImpl extends Disposable implements OfferRepository {
     );
 
     final QueryResult result = await client.mutate(options);
-    print(result.data.toString());
     PurchaseModel purchaseModel = PurchaseModel.fromJson(await result.data["purchase"]);
+
+    if(purchaseModel.success) {
+      var historic = Hive.box('historic');
+      historic.add(offer);
+    }
+
     return purchaseModel;
   }
 
