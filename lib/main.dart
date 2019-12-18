e67a3c3b224c13ec130079c0694b4424951f8add
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 09:24:29 -0300

    Added hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive/hive.dart';
 import 'package:nubuy/app/app_module.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
 void main() async {
   await DotEnv().load('.env');
+
+  final applicationDirectory = await path_provider.getApplicationDocumentsDirectory();
+
+  Hive.init(applicationDirectory.path);
 
   runApp(AppModule());
 }
