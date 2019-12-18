0ce5d20200c38337f9271cfaa4da9cac1a66328c
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 03:18:08 -0300

    Put raised button more generic with color

--- lib/app/shared/widgets/custom_raised_button.dart
+++ lib/app/shared/widgets/custom_raised_button.dart
@@ -8,16 +8,17 @@ class CustomRaisedButton extends StatefulWidget {
   final String raisedText;
   final Color raisedTextColor;
   final double raisedFontSize;
+  final double widthRaised;
 
-  const CustomRaisedButton({
-      @required this.onPressedRaisedButton,
+  const CustomRaisedButton(
+      {@required this.onPressedRaisedButton,
       @required this.raisedText,
+        this.widthRaised = .8,
       this.raisedButtonColor = CustomColors.mainGreen,
       this.distanceManyButton = 10,
       this.raisedTextColor = CustomColors.mainWhite,
-      this.raisedFontSize = 18
-  });
-  
+      this.raisedFontSize = 18});
+
   @override
   _CustomRaisedButtonState createState() => _CustomRaisedButtonState();
 }
@@ -26,20 +27,19 @@ class _CustomRaisedButtonState extends State<CustomRaisedButton> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-            padding: EdgeInsets.only(top: widget.distanceManyButton),
-            child: RaisedButton(
-              onPressed: widget.onPressedRaisedButton,
-              color: widget.raisedButtonColor,
-              child: Center(
-                child: Text(
-                  widget.raisedText,
-                  style: TextStyle(
-                    color: widget.raisedTextColor,
-                    fontSize: widget.raisedFontSize
-                  ),
-                ),
-              ),
-            ),
-          );
+      padding: EdgeInsets.only(top: widget.distanceManyButton),
+      child: RaisedButton(
+        onPressed: widget.onPressedRaisedButton,
+        color: widget.raisedButtonColor,
+        child: Center(
+          child: Text(
+            widget.raisedText,
+            style: TextStyle(
+                color: widget.raisedTextColor,
+                fontSize: widget.raisedFontSize),
+          ),
+        ),
+      ),
+    );
   }
 }
