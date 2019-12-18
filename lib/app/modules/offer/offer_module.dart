bd2eb16de629b73831759fdd705e587880924123
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 02:13:33 -0300

    Updated repostiory with implementation in offer module

--- lib/app/modules/offer/offer_module.dart
+++ lib/app/modules/offer/offer_module.dart
@@ -4,19 +4,19 @@ import 'package:nubuy/app/modules/offer/offer_bloc.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:nubuy/app/modules/offer/offer_page.dart';
-import 'package:nubuy/app/modules/offer/offer_repository.dart';
+import 'package:nubuy/app/modules/offer/impl/offer_repository_impl.dart';
 
 class OfferModule extends ModuleWidget {
   @override
   List<Bloc> get blocs => [
         Bloc((i) => OfferBloc(
-          offerRepository: OfferModule.to.getDependency<OfferRepository>()
+          offerRepository: OfferModule.to.getDependency<OfferRepositoryImpl>()
         )),
       ];
 
   @override
   List<Dependency> get dependencies => [
-    Dependency((i) => OfferRepository(
+    Dependency((i) => OfferRepositoryImpl(
       client: AppModule.to.getDependency<GraphQLClient>()
     ))
   ];
