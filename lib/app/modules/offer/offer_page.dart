972acc5f4102d8fe6ef13ee04281dda3c0146240
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 17:32:16 -0300

    Changed offer page

--- lib/app/modules/offer/offer_page.dart
+++ lib/app/modules/offer/offer_page.dart
@@ -1,5 +1,6 @@
 import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:nubuy/app/modules/offer/widgets/offer_body_page.dart';
 import 'package:nubuy/app/shared/colors.dart';
 import 'package:nubuy/app/shared/widgets/mobile_offline.dart';
@@ -12,14 +13,26 @@ class OfferPage extends StatefulWidget {
 class _OfferPageState extends State<OfferPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: CustomColors.mainSky,
-      body: StreamBuilder<ConnectivityResult>(
-        stream: Connectivity().onConnectivityChanged,
-        builder: (BuildContext context, AsyncSnapshot<ConnectivityResult> snapshot) {
-          return snapshot.data != ConnectivityResult.none ? OfferBodyPage() : MobileOffline();
-        },
-      ),
+    return FutureBuilder(
+      future: Hive.openBox('historic'),
+      builder: (BuildContext context, snapshot) {
+        if(snapshot.connectionState == ConnectionState.done) {
+          return Scaffold(
+          backgroundColor: CustomColors.mainSky,
+          body: StreamBuilder<ConnectivityResult>(
+            stream: Connectivity().onConnectivityChanged,
+            builder: (BuildContext context,
+                AsyncSnapshot<ConnectivityResult> snapshot) {
+              return snapshot.data != ConnectivityResult.none
+                  ? OfferBodyPage()
+                  : MobileOffline();
+            },
+          ),
+        );
+        } else {
+          return Scaffold();
+        }
+      },
     );
   }
 }
