b6e9a2633aa0a0a70ade4f3a5d9cde314f88a220
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 00:00:37 -0300

    Removed some instances in app_modules

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,9 +1,7 @@
-import 'package:nubuy/app/modules/offer/offer_repository.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:nubuy/app/app_bloc.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
-import 'package:nubuy/app/app_repository.dart';
 import 'package:nubuy/app/app_widget.dart';
 import 'package:nubuy/app/core/constants.dart';
 import 'package:nubuy/app/core/routes.dart';
@@ -16,8 +14,6 @@ class AppModule extends ModuleWidget {
 
   @override
   List<Dependency> get dependencies => [
-        Dependency(
-            (i) => AppRepository(client: i.getDependency<GraphQLClient>())),
         Dependency((i) => ValueNotifier(
               GraphQLClient(
                 cache: InMemoryCache(),
