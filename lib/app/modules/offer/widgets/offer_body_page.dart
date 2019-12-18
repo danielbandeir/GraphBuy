367e3df0f99e251a73772073b20de47726803cb7
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 03:17:35 -0300

    Changed some instances in offer body

--- lib/app/modules/offer/widgets/offer_body_page.dart
+++ lib/app/modules/offer/widgets/offer_body_page.dart
@@ -6,7 +6,7 @@ import 'package:nubuy/app/shared/models/offers.model.dart';
 import 'package:nubuy/app/shared/widgets/custom_alert_dialgog.dart';
 import 'package:nubuy/app/shared/widgets/custom_app_bar.dart';
 import 'package:nubuy/app/shared/widgets/custom_list_tile.dart';
-
+import 'package:nubuy/app/shared/widgets/custom_wait_purchase.dart';
 
 class OfferBodyPage extends StatefulWidget {
   @override
@@ -32,27 +32,42 @@ class _OfferBodyPageState extends State<OfferBodyPage> {
           child: FutureBuilder<List<OfferModel>>(
             future: offerBloc.getOffers(),
             builder: (BuildContext context, snapshot) {
-              return !snapshot.hasData ? Center(child: CircularProgressIndicator(valueColor: const AlwaysStoppedAnimation<Color>(CustomColors.mainWhite))) : Container(
-                decoration: BoxDecoration(
-                    color: CustomColors.thirdaryGrey,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(40),
-                        topRight: Radius.circular(40))),
-                child: Padding(
-                  padding: EdgeInsets.only(left: 20, right: 20, top: 5),
-                  child: ListView.builder(
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return CustomListTile(
-                        onPressed: () => showDescription(context, snapshot.data[index].product.name, snapshot.data[index].price, snapshot.data[index].product.description, snapshot.data[index].product.image, "Comprar", () => offerBloc.purchaseItem(snapshot.data[index].id)),
-                        title: snapshot.data[index].product.name,
-                        price: snapshot.data[index].price,
-                        image: snapshot.data[index].product.image,
-                      );
-                    },
-                  ),
-                ),
-              );
+              return !snapshot.hasData
+                  ? Center(
+                      child: CircularProgressIndicator(
+                          valueColor: const AlwaysStoppedAnimation<Color>(
+                              CustomColors.mainWhite)))
+                  : Container(
+                      decoration: BoxDecoration(
+                          color: CustomColors.thirdaryGrey,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(40),
+                              topRight: Radius.circular(40))),
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 20, right: 20, top: 5),
+                        child: ListView.builder(
+                          itemCount: snapshot.data.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return CustomListTile(
+                              onPressed: () => showDescription(
+                                  context,
+                                  snapshot.data[index].product.name,
+                                  snapshot.data[index].price,
+                                  snapshot.data[index].product.description,
+                                  snapshot.data[index].product.image,
+                                  "Comprar",
+                                  () => Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => CustomWaitPurchase(
+                                    futureAwait: offerBloc.purchaseItem(snapshot.data[index].id),
+                                    routeNameRedirect: "/home/main",
+                                  )))),
+                              title: snapshot.data[index].product.name,
+                              price: snapshot.data[index].price,
+                              image: snapshot.data[index].product.image,
+                            );
+                          },
+                        ),
+                      ),
+                    );
             },
           ),
         ),
