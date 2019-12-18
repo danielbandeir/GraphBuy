dab992be6fdb219b1fe9cc1850576ac5c24b3f11
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-12-18 00:02:57 -0300

    Added home repository and dependencies inside module

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,15 +1,23 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:nubuy/app/app_module.dart';
 import 'package:nubuy/app/modules/home/home_bloc.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:nubuy/app/modules/home/home_page.dart';
+import 'package:nubuy/app/modules/home/home_repository.dart';
 
 class HomeModule extends ModuleWidget {
   @override
-  List<Bloc> get blocs =>
-      [Bloc((i) => HomeBloc())];
+  List<Bloc> get blocs => [
+        Bloc((i) => HomeBloc(
+            homeRepository: HomeModule.to.getDependency<HomeRepository>()))
+      ];
 
   @override
-  List<Dependency> get dependencies => [];
+  List<Dependency> get dependencies => [
+        Dependency((i) =>
+            HomeRepository(client: AppModule.to.getDependency<GraphQLClient>()))
+      ];
 
   @override
   Widget get view => HomePage();
